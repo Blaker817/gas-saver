ab815b1e57fadad4668772b5418931e5e8d7d57a
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2022-01-04 13:02:47 -0600

    added state api

--- script.js
+++ script.js
@@ -11,22 +11,35 @@ async function handle() {
         }
     });
     states = await states.json();
+    states = states.result
     console.log(states)
-    states.result.forEach(function (state) {
+    for (var i = 0; i < states.length; i++) {
+        var state = states[i]
         console.log(state)
-    })
-    console.log(r)
 
-    let zipInfo = await fetch("https://www.zipcodeapi.com/rest/Q9MDb5WDiRm3Fp30Qkrbq94Zhpmk88Bb9mCg1SkddX8UGaNIgQ0kpqZ59g7QpmEy/info.json/76107/degrees", {
-        headers: {
-            "content-type": "application/json",
+        var li = document.createElement('li')
+        li.innerHTML = state.name +': '+state.gasoline
+        var statesList = document.getElementById('states-list')
+        statesList.appendChild(li)
+    }
+    // states.result.forEach(function (state) {
+    //     console.log(state)
+    // })
 
-        },
-        mode: 'no-cors',
-    });
-    zipInfo = await zipInfo.body.json();
-    console.log(zipInfo)
-    console.log(typeof zipInfo)
+    //YOU SHOULD CHANGE THIS TO YOUR API KEY
+    // const owensApiKey = `fcb34b40-62a9-11ec-88d0-1becdba60906`;
+
+    // let apiResponse = await fetch(
+    //     `https://app.zipcodebase.com/api/v1/search?apikey=${owensApiKey}&codes=01073&country=US`
+    // );
+    // apiResponse = await apiResponse.json();
+    // console.log("response", apiResponse);
+
+    // let zipCodeResults = apiResponse.results["01073"];
+    // zipCodeResults = zipCodeResults[0];
+    // const state = zipCodeResults.state_code;
+    // //NOW WE HAVE THE STATE ASSOCIATED WITH THE ZIP CODE THAT WE PUT IN THE FETCH REQUEST
+    // console.log("state", state);
 
 
 }
